b1430c0616db52fc2c1153fdf964b1cd2e007933
Martin Schwill <martin@schwill.art>
2025-04-25 15:04:33 +0200

    moved the questions into the backend repo, created the connection. trying to debug not working for macOS. Working on iOS and in Chrome

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'list_of_symptoms.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+
 
 void main() {
   runApp(const MyApp());
@@ -55,8 +58,29 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final List<String> _items = symptoms; // List of symptoms
+  final List<String> _items = []; // List to store symptoms
   final Set<int> _selectedRows = {}; // Set to keep track of selected rows
+  bool _isLoading = true; // Flag to track loading state
+
+  Future<void> _fetchSymptoms() async {
+  try {    
+      final response = await http.get(Uri.parse('http://192.168.33.12:5001/symptoms'));
+      if (response.statusCode == 200) {
+        final List<dynamic> data = json.decode(response.body);
+        setState(() {
+          _items.clear(); // Clear the list before adding new items
+          _items.addAll(data.map((item) => item['name'] as String).toList());
+          _isLoading = false; // Set loading to false after fetching data
+        });
+      } else {
+        print('Failed to load symptoms');
+        _isLoading = false; // Set loading to false if there's an error
+      }
+    } catch (e) {
+      print('Error fetching symptoms: $e');
+      _isLoading = false; // Set loading to false if there's an error
+    }
+  }
 
   void _onPlusButtonPressed(int index) {
         setState(() {
@@ -68,6 +92,13 @@ class _MyHomePageState extends State<MyHomePage> {
       }
     });
   }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchSymptoms(); // Fetch symptoms when the widget is initialized
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -84,60 +115,63 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         backgroundColor: const Color.fromARGB(255, 185, 250, 110), // Set a custom color for the AppBar
       ),
-      body: Column(
-        children: [
-          // Display the current date above the table
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Align(
-              alignment: Alignment.centerLeft,
-              child: Container(
-                padding: const EdgeInsets.all(8.0),
-                decoration: BoxDecoration(
-                  color: const Color.fromARGB(255, 226, 234, 236),
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                child: Text(
-                  'Data: ${DateTime.now().toLocal().toString().split(' ')[0]}', // Format: YYYY-MM-DD
-                  style: Theme.of(context).textTheme.titleMedium,
-                ),
-            ),
-            ), 
-          ),
-        Expanded(child: ListView.builder(
-        itemCount: _items.length,
-        itemBuilder: (context, index) {
-          final isSelected = _selectedRows.contains(index); // Check if the row is selected
-          return Padding(
-            padding: const EdgeInsets.fromLTRB(60.0, 10.0, 40.0, 10.0),
-            child: Container(
-                padding: const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
-                decoration: BoxDecoration(
-                    color: isSelected ? const Color.fromARGB(255, 247, 239, 162) : Colors.transparent, // Change background color if selected
-                    borderRadius: BorderRadius.circular(10.0), // Rounded edges
-                ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Flexible(
-                    child: Text(
-                      _items[index],
-                      style: Theme.of(context).textTheme.bodyLarge,
-                      softWrap: true, // Allow text to wrap,
-                      overflow: TextOverflow.visible, // Show overflow text
-                    ),
+      body: _isLoading
+          ? const Center(child: CircularProgressIndicator(),)
+      
+        : Column(
+          children: [
+            // Display the current date above the table
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Container(
+                  padding: const EdgeInsets.all(8.0),
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 226, 234, 236),
+                    borderRadius: BorderRadius.circular(10.0),
                   ),
-                  
-                  IconButton(
-                    icon: const Icon(Icons.add),
-                    onPressed: () => _onPlusButtonPressed(index),
-                ),
-              ],
+                  child: Text(
+                    'Data: ${DateTime.now().toLocal().toString().split(' ')[0]}', // Format: YYYY-MM-DD
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+              ),
+              ), 
             ),
-          ));
-        },
-      ),
-      ),
+          Expanded(child: ListView.builder(
+          itemCount: _items.length,
+          itemBuilder: (context, index) {
+            final isSelected = _selectedRows.contains(index); // Check if the row is selected
+            return Padding(
+              padding: const EdgeInsets.fromLTRB(60.0, 10.0, 40.0, 10.0),
+              child: Container(
+                  padding: const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
+                  decoration: BoxDecoration(
+                      color: isSelected ? const Color.fromARGB(255, 247, 239, 162) : Colors.transparent, // Change background color if selected
+                      borderRadius: BorderRadius.circular(10.0), // Rounded edges
+                  ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Flexible(
+                      child: Text(
+                        _items[index],
+                        style: Theme.of(context).textTheme.bodyLarge,
+                        softWrap: true, // Allow text to wrap,
+                        overflow: TextOverflow.visible, // Show overflow text
+                      ),
+                    ),
+                    
+                    IconButton(
+                      icon: const Icon(Icons.add),
+                      onPressed: () => _onPlusButtonPressed(index),
+                  ),
+                ],
+              ),
+            ));
+          },
+        ),
+        ),
       Padding(
         padding: const EdgeInsets.all(32.0),
         child: Row(
